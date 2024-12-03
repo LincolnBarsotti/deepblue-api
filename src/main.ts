cc2f977a371f8aa0ef8a966c70108e4de4af46fb
Lincoln Barsotti <107706237+LincolnBarsotti@users.noreply.github.com>
2024-12-03 11:26:41 -0300

    :sparkles: Feat(cors): habilitar CORS globalmente para todas as origens

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Habilitando CORS
+  app.enableCors({
+    origin: '*', // Permite todas as origens (você pode restringir a origens específicas aqui)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
+    allowedHeaders: 'Content-Type, Accept, Authorization', // Cabeçalhos permitidos
+  });
+
   const config = new DocumentBuilder()
     .setTitle('UPX - V: DEEPBLUE')
     .setDescription(
-      'O deepblue é uma aplicação feita para empresas que buscam o gerenciamento de seus incidentes e acidentes nas plataformas de petroleo',
+      'O deepblue é uma aplicação feita para empresas que buscam o gerenciamento de seus incidentes e acidentes nas plataformas de petróleo',
     )
     .setVersion('1.0')
     .build();
